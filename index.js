b8086051a2f76e8bd584e28782be41617673a71e
choufi <80413752+choufi@users.noreply.github.com>
2023-10-11 18:22:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// INDEX SECCIÓN PRIMERA
+
+//para el fade in de los elementos de la sección
+$(document).ready(function() {
+  $(".header-photo").hide();
+  $(".header-photo").fadeIn(3000);
+  $(".div-text").hide();
+  $(".div-text").fadeIn(3000);
+})
 
 // al pasar con el ratón por encima de cualquier imagen del header, se devuleve la misma imagen pero con color
- $(".header-photo").on("mouseover", function() {
+$(".header-photo").on("mouseover", function() {
       var idSelectedImage = $(this).attr("id");
       $(this).attr("src", "./Assets/Images/" + idSelectedImage + "-original.png");     })
 
@@ -10,6 +19,25 @@ $(".header-photo").on("mouseout", function() {
   $(this).attr("src", "./Assets/Images/" + idSelectedImage + ".png");
  })
 
+// INDEX SECCIÓN SEGUNDA
+
+//para el fade in de los elementos de la sección
+$(".my-photo").hide();
+$(document).scroll(function() {
+  $(".my-photo").fadeIn(3000);
+});
+
+
+
+// INDEX SECCIÓN TERCERA
+
+//para el fade in de los elementos de la sección
+$(".titulo-seccion").hide();
+$(".iconos-seccion").hide();
+$(document).scroll(function() {
+  $(".titulo-seccion").fadeIn(3000);
+  $(".iconos-seccion").fadeIn(4000);
+});
 
  // al pasar con el ratón por encima de los iconos de la sección "mis trabajos", se devuleve la misma imagen pero con color rosa
  $(".iconos").on("mouseover", function() {
@@ -20,4 +48,7 @@ $(".header-photo").on("mouseout", function() {
 $(".iconos").on("mouseout", function() {
   var idSelectedImage = $(this).attr("id");
   $(this).attr("src", "Assets/Images/Iconos peluqeria/" + idSelectedImage + "-verde.png");     })
+
+
+
 
